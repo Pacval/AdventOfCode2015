425e4b3bd73d51712af73e7c4fdb0e4713f4ded9
Pacval <val.rochet@gmail.com>
2019-01-10 17:48:21 +0100

    day 10

---
+++ src/fr/rochet/days/Day10.java
@@ -0,0 +1,48 @@
+package fr.rochet.days;
+
+import fr.rochet.DayInterface;
+
+import java.io.IOException;
+
+public class Day10 implements DayInterface {
+
+    @Override
+    public void part1() throws IOException {
+        process(40);
+    }
+
+    @Override
+    public void part2() throws IOException {
+        process(50);
+    }
+
+    private void process(int rounds) {
+        String entry = "3113322113";
+
+        String currentSeq = entry;
+
+        for (int i = 0; i < rounds; i ++) {
+            StringBuilder nextSeq = new StringBuilder();
+            Character previousChar = null;
+            int count = 1;
+            for (char c : currentSeq.toCharArray()) {
+                if (previousChar == null) {
+                    previousChar = c;
+                } else if (previousChar == c) {
+                    count++;
+                } else {
+                    nextSeq.append(count);
+                    nextSeq.append(previousChar);
+                    previousChar = c;
+                    count = 1;
+                }
+            }
+            nextSeq.append(count);
+            nextSeq.append(previousChar);
+
+            currentSeq = nextSeq.toString();
+        }
+
+        System.out.println("Length of sequence for " + rounds + " rounds : " + currentSeq.length());
+    }
+}
