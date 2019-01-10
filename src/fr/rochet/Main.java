a9de0616d9e4596c940aa201b52bb7bd6511a254
Pacval <val.rochet@gmail.com>
2019-01-10 17:53:35 +0100

    day 22

--- src/fr/rochet/Main.java
+++ src/fr/rochet/Main.java
@@ -1,12 +1,12 @@
 package fr.rochet;
 
-import fr.rochet.days.Day21;
+import fr.rochet.days.Day22;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
-            DayInterface day = new Day21();
+            DayInterface day = new Day22();
             day.part1();
             day.part2();
         } catch (Exception e) {
